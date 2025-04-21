ca6d3852c9049c33e0bc8bbd736d0f9861924a2e
ambuj-jainstalin <106435722+ambuj-jainstalin@users.noreply.github.com>
2025-04-21 14:11:47 +0530

    Update BankLogo.tsx

--- src/components/BankLogo.tsx
+++ src/components/BankLogo.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const BankLogo: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <img 
-      src="/lovable-uploads/cfed4b12-413a-446f-9085-adcf9de91b60.png" 
+      src="/lovable-uploads/Emirates-Islamic-Bank-logo-1024x537.jpeg" 
       alt="ADCB Bank"
       className={className}
     />
